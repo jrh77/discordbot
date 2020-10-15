b580859ac16eae025d8ba9214b094448ca2f3676
Junrong <enryulol@gmail.com>
2020-10-14 20:36:14 -0400

    tour message

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 const prefix = "!"
 
