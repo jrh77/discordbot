75af0651a46d45ab8876724c84bb341d44b3677a
Junrong <enryulol@gmail.com>
2020-10-16 00:00:41 -0400

    1234

--- bot.js
+++ bot.js
@@ -18,5 +18,18 @@ client.on('message', message => {
    }  
 })
 
+client.on('message', message => {
+   if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+})
+
 client.login(config.token);
 
