fa570b2920bc399523a24a18dc5dbb9a55eeb653
Junrong <enryulol@gmail.com>
2020-10-19 22:17:24 -0400

    index.js bot update

--- index.js
+++ index.js
@@ -13,11 +13,22 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	} else if (command === 'beep') {
-		message.channel.send('Boop.');
+	if (command === 'add') {
+		var date = new Date();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		var year = date.getFullYear();
+		var formattedDate = `${month}-${day}-${year} `;
+
+		let tasks = {"Today\'s Date" : formattedDate};
+
+		message.channel.send(JSON.stringify(args + " has been added! Here are your tasks for today."));
+		message.channel.send(JSON.stringify(tasks));	
+		//	message.channel.send(JSON.stringify(args)); Doesn't save args, just sends current args
 	}
+	else if (command === 'show') {
+	}
+ 
 	// other commands...
 });
 
